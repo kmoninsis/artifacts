1de10720b06361546bf2f4b39aced29a08f11b38
Simon Niks <simonniks@gmail.com>
2025-02-16 17:31:17 +0100

    update Method script

--- static/js/method.js
+++ static/js/method.js
@@ -1,78 +1,47 @@
 let text = "My method of investigation is like Bas Jan Ader's Falling"
 let dancingChars = [];
-let totalW = 150;
+//let totalW = 150;
 let w;
 let spannedChar;
 let spanArray = [];
 let col = ['#8ae0c5','#a5e8db','#95c8d4','#90a8c9','#b3abd6'];
-let test = [];
-class DanceSpan {
-  constructor(element, x, y) {
-    element.position(x, y); 
-	element.style('margin-left',(totalW-w) + 'px');
-	element.style('color', random(col)); 
-    if(element.html() === '\u0020'){      	 	
-    	// print(totalW, w);
-    	totalW += 10;
-    }
-    this.element = element;
-    this.x = x;
-    this.y = y;
-  }
+let activatedChars = [];
 
-  brownian(y) {
-    
-    if(this.y < windowHeight-50){
-    	this.y += random(y);
-    }
-    this.element.position(this.x, this.y);
-
-  }
-  stop(){
-  	this.y = windowHeight;
-  	this.element.position(this.x,this.y);
-  }
-}
 
 function setup(){
-	createP(text).addClass('text').hide();
-	let texts = selectAll('.text');
-
-	const words = texts[0].html();
-	const chars = split(words, '');
-	for(x in chars){
-		spannedChar = createSpan(chars[x]);
-		append(spanArray, spannedChar);
-		// print(spannedChar.elt.clientWidth);
-		w = spannedChar.elt.clientWidth;
-		totalW += w;
-		spannedChar.style('margin-left','10px');
-		const dw = new DanceSpan(spannedChar, x, 50);
-		dancingChars.push(dw);
-		append(test, 0);
+	let container = createElement('h2');
+	container.style('pointerEvents:auto;display:flex;width:100%;textAlign:justify;justifyContent:space-evenly;position:absolute;paddingLeft:50px;paddingRight:50px;whiteSpace:pre');
+	const chars = split(text, '');
+	for(index in chars){
+		spannedChar = createSpan(chars[index]);
+		spannedChar.style('color', random(col));
+		spannedChar.style('paddingTop', '0px');
+		spannedChar.style('height','min-content');
+		Object.defineProperty(spannedChar,'index', {value: index});
+		spanArray.push(spannedChar);
+		container.child(spannedChar);
+		activatedChars.push(0);
 	}
+
 	for(x in spanArray){
 		spanArray[x].mouseOver(function(){
-			test[this.x] = 1;
-			// print(test[this.x]);			
-		});			
+			print(this.index)
+			activatedChars[this.index] = 1;
+		});
 	}
-	
 }
 
 function draw(){
-
-	for(x in dancingChars){
-//		print(test[x]);
-			if(test[x] == 1){
-				dancingChars[x].brownian(5);
-				
-			}		
-				
-		
-
-			
+	for(x in spanArray){
+		if(activatedChars[x] == 1){
+			let char = spanArray[x];
+			if(int(char.elt.style.paddingTop) < windowHeight -50){
+				let newHeight = int(char.elt.style.paddingTop) + random(5);
+				char.elt.style.paddingTop = `${newHeight}px`;
+			} else {
+			char.elt.style.paddingTop = `${windowHeight - 50}px`;
+			}
+		}
 	}
-
 }
 
